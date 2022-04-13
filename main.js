5ffcaf62f42e57df5b6fc84bf1551e42f340eb92
Ashif <ashif51@gmail.com>
2022-04-13 09:18:12 +0530

    2nd day

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ $(window).scroll(function(){
     $('.navbar').toggleClass("nav-bg",$(window).scrollTop() > 40);
    
 });
+
+// Typed animation
+var typed = new Typed('#typed', {
+    
+    strings: [
+      'Welcome Here',
+      'We Have Years of exprience',
+      'Quick Services',
+    ],
+
+    typeSpeed: 80,
+    backSpeed: 60,
+    loop: true,
+    showCursor: true,
+});
